b490cebf2947bce21be0f19d97bae07bbfde1f6d
Sadhvi kesarwani <Sadhvikesarwani1@gmail.com>
2024-07-18 15:35:08 +0530

    simple calc

---
+++ DOM For JS/011. project/5.SimpleCalc/script.js
@@ -0,0 +1,24 @@
+document.getElementById("btn").addEventListener("click", () => {
+  //Get elements from the Dom
+  let num1 = document.querySelector(".num1").value;
+  let num2 = document.querySelector(".num2").value;
+  let result = document.querySelector(".result");
+  let operator = document.getElementById("selectOp").value;
+
+  switch (operator) {
+    case "plus":
+      result.innerHTML = Number(num1) + Number(num2);
+      break;
+
+    case "min":
+      result.innerHTML = Number(num1) - Number(num2);
+      break;
+    case "div":
+      result.innerHTML = Number(num1) / Number(num2);
+      break;
+
+    case "mul":
+      result.innerHTML = Number(num1) * Number(num2);
+      break;
+  }
+});
