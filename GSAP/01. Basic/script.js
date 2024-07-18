67d348dc70e787d49dd22a3c6d90facfe3105027
Sadhvi kesarwani <Sadhvikesarwani1@gmail.com>
2024-07-18 19:29:20 +0530

    with timeline

--- GSAP/01. Basic/script.js
+++ GSAP/01. Basic/script.js
@@ -1,3 +1,5 @@
+//simple animation in div(box)
+
 // gsap.to("#box1", {
 //   //gsap.to when we want element to move initial to final
 //   x: 1327,
@@ -18,6 +20,8 @@ gsap.to("#box2", { x: 900, duration: 2, delay: 1 });
 //   borderRadius: "50%",
 // });
 
+//Animation in text
+
 // gsap.from("h1", {
 //   opacity: 0,
 //   color: "yellow",
@@ -27,27 +31,51 @@ gsap.to("#box2", { x: 900, duration: 2, delay: 1 });
 //   stagger: 0.5, //bhai ak ak krke aaiga
 // });
 
-gsap.to("#box1", {
-  x: 1327,
+//Some more Animation in box but problem arise due to time managament between elements for animation
+
+// gsap.to("#box1", {
+//   x: 1327,
+//   backgroundColor: "blue",
+//   rotate: 360,
+//   duration: 1.5,
+//   delay: 1,
+// });
+
+// gsap.to("#box2", {
+//   x: 1327,
+//   backgroundColor: "green",
+//   duration: 1.5,
+//   delay: 2.5, //1.5 + 1 = 2.5
+// });
+
+// gsap.to("#box3", {
+//   x: 1327,
+//   backgroundColor: "crimson",
+//   borderRadius: "50%",
+//   rotate: 360,
+//   scale: 0.5,
+//   duration: 1.5,
+//   delay: 4, //1.5 + 2.5 = 4
+// });
+
+//Animation in box with timeline
+
+var tl = gsap.timeline();
+
+tl.to("#box1", {
+  x: 1200,
   backgroundColor: "blue",
   rotate: 360,
   duration: 1.5,
   delay: 1,
 });
 
-gsap.to("#box2", {
-  x: 1327,
-  backgroundColor: "green",
+tl.to("#box2", {
+  x: 1200,
   duration: 1.5,
-  delay: 2.5, //1.5 + 1 = 2.5
 });
 
-gsap.to("#box3", {
-  x: 1327,
-  backgroundColor: "crimson",
-  borderRadius: "50%",
-  rotate: 360,
-  scale: 0.5,
+tl.to("#box3", {
+  x: 1200,
   duration: 1.5,
-  delay: 4, //1.5 + 2.5 = 4
 });
