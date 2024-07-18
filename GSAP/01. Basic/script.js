341ad666c4cb6161b2c0e6591794f7a92e0a22da
Sadhvi kesarwani <Sadhvikesarwani1@gmail.com>
2024-07-18 18:11:45 +0530

    basic Text animation

--- GSAP/01. Basic/script.js
+++ GSAP/01. Basic/script.js
@@ -1,28 +1,33 @@
-gsap.to("#box1", {
-  //gsap.to when we want element to move initial to final
-  x: 1327,
-  duration: 2,
-  delay: 1,
-  rotate: 360,
-  backgroundColor: "blue",
-  borderRadius: "50%",
-});
-
-// gsap.to("#box2", {
-//   x: 900,
+// gsap.to("#box1", {
+//   //gsap.to when we want element to move initial to final
+//   x: 1327,
 //   duration: 2,
 //   delay: 1,
+//   rotate: 360,
+//   backgroundColor: "blue",
+//   borderRadius: "50%",
 // });
-gsap.from("#box2", {
-  //gsap.from is used when we want element to move final to initial
-  x: 1200,
-  duration: 2,
+
+// // gsap.to("#box2", {
+// //   x: 900,
+// //   duration: 2,
+// //   delay: 1,
+// // });
+// gsap.from("#box2", {
+//   //gsap.from is used when we want element to move final to initial
+//   x: 1200,
+//   duration: 2,
+//   delay: 1,
+//   rotate: 360,
+//   backgroundColor: "green",
+//   borderRadius: "50%",
+// });
+
+gsap.from("h1", {
+  opacity: 0,
+  color: "yellow",
+  Duration: 1,
+  y: 30, //y-axis
   delay: 1,
-  rotate: 360,
-  backgroundColor: "green",
-  borderRadius: "50%",
+  stagger: 0.5, //bhai ak ak krke aaiga
 });
-
-// gsap.to("#box"){
-
-// }
