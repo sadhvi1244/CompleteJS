81655afbfa465c28cef177c39155700452dcd8b9
Sadhvi kesarwani <Sadhvikesarwani1@gmail.com>
2024-07-18 19:05:14 +0530

    problem arise due to time managament btween elements

--- GSAP/01. Basic/script.js
+++ GSAP/01. Basic/script.js
@@ -7,12 +7,7 @@
 //   backgroundColor: "blue",
 //   borderRadius: "50%",
 // });
-
-// // gsap.to("#box2", {
-// //   x: 900,
-// //   duration: 2,
-// //   delay: 1,
-// // });
+gsap.to("#box2", { x: 900, duration: 2, delay: 1 });
 // gsap.from("#box2", {
 //   //gsap.from is used when we want element to move final to initial
 //   x: 1200,
@@ -23,11 +18,36 @@
 //   borderRadius: "50%",
 // });
 
-gsap.from("h1", {
-  opacity: 0,
-  color: "yellow",
-  Duration: 1,
-  y: 30, //y-axis
+// gsap.from("h1", {
+//   opacity: 0,
+//   color: "yellow",
+//   Duration: 1,
+//   y: 30, //y-axis
+//   delay: 1,
+//   stagger: 0.5, //bhai ak ak krke aaiga
+// });
+
+gsap.to("#box1", {
+  x: 1327,
+  backgroundColor: "blue",
+  rotate: 360,
+  duration: 1.5,
   delay: 1,
-  stagger: 0.5, //bhai ak ak krke aaiga
+});
+
+gsap.to("#box2", {
+  x: 1327,
+  backgroundColor: "green",
+  duration: 1.5,
+  delay: 2.5, //1.5 + 1 = 2.5
+});
+
+gsap.to("#box3", {
+  x: 1327,
+  backgroundColor: "crimson",
+  borderRadius: "50%",
+  rotate: 360,
+  scale: 0.5,
+  duration: 1.5,
+  delay: 4, //1.5 + 2.5 = 4
 });
